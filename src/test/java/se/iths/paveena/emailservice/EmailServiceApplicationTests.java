68cde2936983c9d626d295bfd18a4bf31af55d5e
paveena-naebsuwan <paveena.naebsuwan@gmail.com>
2026-05-19 13:22:27 +0200

    Initial commit

---
+++ src/test/java/se/iths/paveena/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.paveena.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
