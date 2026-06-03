dfda2a76769fa2fdf3bc9eb6c168651eaa95304b
paveena-naebsuwan <paveena.naebsuwan@gmail.com>
2026-06-03 11:04:00 +0200

    Revert "Merge pull request #23 from paveena-naebsuwan/feature-subscriber"
    This reverts commit b651f47bfb28fa97010ccb289fc7c75f6285317e, reversing
    changes made to 66d32096e2ca76228d14eb9bf0a80467230e5be7.

--- src/main/java/se/iths/paveena/emailservice/subscriber/EmailSubscriber.java
+++ src/main/java/se/iths/paveena/emailservice/subscriber/EmailSubscriber.java
@@ -4,19 +4,60 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 import se.iths.paveena.emailservice.dto.OrderResponse;
-import se.iths.paveena.emailservice.service.OrderConfirmationService;
+import se.iths.paveena.emailservice.dto.ProductInfoResponse;
+import se.iths.paveena.springmessenger.messaging.EmailSender;
+import se.iths.paveena.springmessenger.model.Email;
+
+import java.math.BigDecimal;
 
 @Component
 @RequiredArgsConstructor
 public class EmailSubscriber {
 
-    private final OrderConfirmationService orderConfirmationService;
+    private final EmailSender emailSender;
 
     @RabbitListener(queues = "${rabbitmq.queue.name}")
     public void subscribe(OrderResponse order) {
 
-        System.out.println("Order mottagen: #" + order.id());
+        System.out.println("Order mottagen: " + order.id());
 
-        orderConfirmationService.sendOrderConfirmation(order);
+        Email email = new Email();
+        email.setRecipient(order.customerName());
+        email.setSubject("Orderbekräftelse: ");
+        email.setMessage(orderConfirm(order));
+
+        emailSender.send(email);
+    }
+
+    private String orderConfirm(OrderResponse order) {
+
+        StringBuilder orderBuilder = new StringBuilder();
+
+        orderBuilder
+                .append("Tack för din beställning!\nOrdernummer: #")
+                .append(order.id())
+                .append("\n\n");
+
+        for (ProductInfoResponse item : order.items()) {
+
+            BigDecimal totalPerItem = item.price().multiply(BigDecimal.valueOf(item.quantity()));
+
+            orderBuilder
+                    .append(item.name())
+                    .append(" ")
+                    .append(item.price())
+                    .append(" kr x ")
+                    .append(item.quantity())
+                    .append(" = ")
+                    .append(totalPerItem)
+                    .append(" kr\n");
+        }
+
+        orderBuilder
+                .append("\nTotalt: ")
+                .append(order.totalPrice())
+                .append(" kr\n\nHälsningar från Webbhandlaren!");
+
+        return orderBuilder.toString();
     }
 }
